34a4d47956743e6082311d5f6766d83c187c97b0
imamalirabbani <imamalirabbani@gmail.com>
2026-05-08 00:04:48 +0700

    debug: add entry point error reporting for vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
-const app = require('../backend/server');
-
-// Vercel serverless functions handle the export as the handler
-module.exports = app;
+try {
+    const app = require('../backend/server');
+    module.exports = app;
+} catch (err) {
+    console.error("Vercel Entry Error:", err);
+    module.exports = (req, res) => {
+        res.status(500).send(`
+            <h1>Vercel Entry Error</h1>
+            <p><strong>Message:</strong> ${err.message}</p>
+            <pre>${err.stack}</pre>
+        `);
+    };
+}
